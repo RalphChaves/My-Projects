0f66773268f19fdb4142d44e0a5b555c2b40ac22
RalphChaves <89053803+RalphChaves@users.noreply.github.com>
2021-08-24 15:31:11 +0000

    Initial commit

---
+++ Project 01/projectDetective.js
@@ -0,0 +1,20 @@
+/*Faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As
+perguntas são:
+1. "Telefonou para a vítima?"
+2. "Esteve no local do crime?"
+3. "Mora perto da vítima?"
+4. "Devia para a vítima?"
+5. "Já trabalhou com a vítima?"
+O programa deve no final emitir uma classificação sobre a participação da
+pessoa no crime.
+Se a pessoa responder positivamente a 2 questões ela deve ser classificada
+como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso
+contrário, ele será classificado como "Inocente".*/
+
+console.logk(Sou o Detetive, Sherlock estou aqui para te fazer algumas perguntas?);
+
+let resp1 = prompt("1. Telefonou para a vítima?");
+let resp2 = prompt("2. "Esteve no local do crime?");
+let resp3 = prompt("3. "Mora perto da vítima?");
+let resp4 = prompt("4. "Devia para a vítima?");
+let resp5 = prompt("5. "Já trabalhou com a vítima?");
